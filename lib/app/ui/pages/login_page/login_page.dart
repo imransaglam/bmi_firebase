584d8a675fc7e2469306931231e67fa5356a3342
imransaglam <imransaglam9@gmail.com>
2023-01-01 18:21:06 +0300

    third commit

--- lib/app/ui/pages/login_page/login_page.dart
+++ lib/app/ui/pages/login_page/login_page.dart
@@ -1,47 +1,57 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../../widget/custom_tabBar_widget.dart';
 import '../../../controllers/login_controller.dart';
 
-
 class LoginPage extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
- return Scaffold(
+    return Scaffold(
       backgroundColor: Color(0xffFF9292),
       body: SingleChildScrollView(
         child: Column(
           children: [
-           Stack(
-             children: [
-               Container(
-                height: 250,
-                 width: MediaQuery.of(context).size.width,),  
-                 Positioned(bottom: 40,left:20,child: Text('Hey!',style:Theme.of(context).textTheme.headlineMedium?.copyWith(fontFamily: 'Poppins',fontWeight: FontWeight.w600,color: Color(0xffFFDCDC)),)),
-                 Positioned(bottom:0,left:20,child: Text('Welcome Back',style:Theme.of(context).textTheme.headlineMedium?.copyWith(fontFamily: 'Poppins',fontWeight: FontWeight.w600,color: Color(0xffFFDCDC)),)),
-             ],
-           ),
-      
-          //  Padding(
-          //    padding: const EdgeInsets.only(top:20),
-          //    child: Container(
-          //     height:MediaQuery.of(context).size.height,
-          //     width: MediaQuery.of(context).size.width,
-          //     decoration: BoxDecoration(
-          //       borderRadius: BorderRadius.horizontal(left:Radius.circular(40),right: Radius.circular(40)),
-          //     color: Color(0xffFFB4B4)
-          //     ),
-          //   child: CustomTabBarWidget(),
-          //  ),
-          //  ),
-
-           CustomTabBarWidget()
-          
+            Stack(
+              children: [
+                Container(
+                  height: 300,
+                  width: MediaQuery.of(context).size.width,
+                ),
+                Positioned(
+                    bottom: 45,
+                    left: 20,
+                    child: Text(
+                      'Hey!',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineMedium
+                          ?.copyWith(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xffFFDCDC)),
+                    )),
+                Positioned(
+                    bottom: 0,
+                    left: 20,
+                    child: Text(
+                      'Welcome Back',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineMedium
+                          ?.copyWith(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xffFFDCDC)),
+                    )),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CustomTabBarWidget()
           ],
         ),
       ),
     );
   }
 }
-  
--- lib/widget/custom_tabBar_widget.dart
+++ lib/widget/custom_tabBar_widget.dart
@@ -9,83 +9,271 @@ class CustomTabBarWidget extends StatefulWidget {
   State<CustomTabBarWidget> createState() => _CustomTabBarWidgetState();
 }
 
-class _CustomTabBarWidgetState extends State<CustomTabBarWidget> with TickerProviderStateMixin{
-  
+class _CustomTabBarWidgetState extends State<CustomTabBarWidget>
+    with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-   
-   TabController _tabController=TabController(length: 2,vsync: this);
-    return 
-       Container(
-          height:MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.horizontal(left:Radius.circular(40),right: Radius.circular(40)),
-              color: Color(0xffFFDCDC)
-              ),
-        child: Column(
-          children: [
-            Container(
-              child:  Align(
+    TabController _tabController = TabController(length: 2, vsync: this);
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+          color: Color(0xffFFDCDC)),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Container(
+              child: Align(
                 alignment: Alignment.center,
                 child: TabBar(
-                  indicator: UnderlineTabIndicator(
-            borderSide: BorderSide(width: 2.0,color: Color(0xffff9292)),
-            insets: EdgeInsets.symmetric(horizontal:50.0)
-          ),
-                  indicatorColor: Colors.amber,
-                  labelColor: Color(0xffff9292),
-                  controller:_tabController ,
-                  tabs: [
-                  Tab(child: Text('Login',style:TextStyle(fontFamily: 'Poppins',fontSize:20,fontWeight: FontWeight.w600 ),),),
-                  Tab(child: Text('Sign Up',style:TextStyle(fontFamily: 'Poppins',fontSize:20,fontWeight: FontWeight.w600 ),),)
-                ]),
+                    indicator: UnderlineTabIndicator(
+                        borderSide:
+                            BorderSide(width: 2.0, color: Color(0xffff9292)),
+                        insets: EdgeInsets.symmetric(horizontal: 50.0)),
+                    indicatorColor: Colors.amber,
+                    labelColor: Color(0xffff9292),
+                    unselectedLabelColor: Color(0xffff9292),
+                    controller: _tabController,
+                    tabs: [
+                      Tab(
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                      Tab(
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      )
+                    ]),
               ),
             ),
-            Container(
-               width: double.infinity,
-               height: 300,
-              child:  TabBarView(
-                controller:_tabController ,
-                children:[
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 40,vertical: 40),
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Email"),
-                        Padding(
-                          padding: const EdgeInsets.only(top:15,bottom:15),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(60))
-                              )
-                            ),
-                          ),
-                        ),
-                          Text("Password"),
-                        Padding(
-                          padding: const EdgeInsets.only(top:15,bottom:15),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(60))
-                              )
-                            ),
-                          ),
-                        )
-                      ],
-                    )
+          ),
+          Container(
+            width: double.infinity,
+            height: 550,
+            child: TabBarView(controller: _tabController, children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+                child: Container(
+                    // color: Colors.amber,
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Email",
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: Color(0xffff9292),
+                          fontFamily: 'Poppins',
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
                     ),
-                  ),
-                  Text("Password"),
-                ] ),
-            )
-          ],
-        ),
-      );
-    
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15, bottom: 15),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 3, color: Color(0xffff9292)),
+                          borderRadius: BorderRadius.all(Radius.circular(60)),
+                        )),
+                      ),
+                    ),
+                    Text(
+                      "Password",
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: Color(0xffff9292),
+                          fontFamily: 'Poppins',
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15, bottom: 15),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 3, color: Color(0xffff9292)),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(60)))),
+                      ),
+                    ),
+                    Center(
+                        child: Padding(
+                      padding: const EdgeInsets.only(top: 40.0),
+                      child: SizedBox(
+                        width: 250,
+                        height: 60,
+                        child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xffff9292),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20))),
+                            child: Text(
+                              "Login",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelLarge
+                                  ?.copyWith(
+                                      color: Color(0xffFFDCDC),
+                                      fontFamily: 'Poppins',
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.w600),
+                            )),
+                      ),
+                    )),
+                    Center(
+                        child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Or sign up here",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall
+                                  ?.copyWith(
+                                      color: Color(0xffff9292),
+                                      fontFamily: 'Poppins',
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w600),
+                            ))),
+                    Center(
+                        child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Terms & Conditions Apply*",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall
+                                  ?.copyWith(
+                                      color: Color(0xffff9292),
+                                      fontFamily: 'Poppins',
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w600),
+                            ))),
+                  ],
+                )
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+                child: Container(
+                    // color: Colors.amber,
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Enter email",
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: Color(0xffff9292),
+                          fontFamily: 'Poppins',
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15, bottom: 15),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 3, color: Color(0xffff9292)),
+                          borderRadius: BorderRadius.all(Radius.circular(60)),
+                        )),
+                      ),
+                    ),
+                    Text(
+                      "Create password",
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: Color(0xffff9292),
+                          fontFamily: 'Poppins',
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15, bottom: 15),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 3, color: Color(0xffff9292)),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(60)))),
+                      ),
+                    ),
+                    Center(
+                        child: Padding(
+                      padding: const EdgeInsets.only(top: 40.0),
+                      child: SizedBox(
+                        width: 250,
+                        height: 60,
+                        child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xffff9292),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20))),
+                            child: Text(
+                              "Sign Up",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelLarge
+                                  ?.copyWith(
+                                      color: Color(0xffFFDCDC),
+                                      fontFamily: 'Poppins',
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.w600),
+                            )),
+                      ),
+                    )),
+                    Center(
+                        child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Or login here",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall
+                                  ?.copyWith(
+                                      color: Color(0xffff9292),
+                                      fontFamily: 'Poppins',
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w600),
+                            ))),
+                    Center(
+                        child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Terms & Conditions Apply*",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall
+                                  ?.copyWith(
+                                      color: Color(0xffff9292),
+                                      fontFamily: 'Poppins',
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w600),
+                            ))),
+                  ],
+                )
+                ),
+              ),
+            ]),
+          )
+        ],
+      ),
+    );
   }
 }
